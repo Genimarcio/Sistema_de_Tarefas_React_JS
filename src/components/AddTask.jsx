4bed6a9602e02f0c8188ab0c0a43ab0ebb6862e7
Genimarcio <genimarcio.silva@gmail.com>
2022-01-20 19:21:24 -0300

    :recycle: Código Refatorado

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
-import './AddTask.css';
+import React, { useState } from "react";
+
 import Button from "./Button";
 
-const AddTask = ({handleTaskAddition}) => {
-    const [inputData, setInputData] = useState('');
+import "./AddTask.css";
 
-    const handleInputChange = (e) => {
-        setInputData(e.target.value);
-    };
+const AddTask = ({ handleTaskAddition }) => {
+	const [inputData, setInputData] = useState("");
 
-    const handleAddTaskClick = () => {
-        handleTaskAddition(inputData)
-        setInputData('');
-    }
+	const handleInputChange = (e) => {
+		setInputData(e.target.value);
+	};
 
-    return (
-        <div className='add-task-container'>
-            <input 
-            onChange={handleInputChange} 
-            value={inputData}
-            className='add-task-input' 
-            type='text' 
+	const handleAddTaskClick = () => {
+		handleTaskAddition(inputData);
+		setInputData("");
+	};
 
-            />;
-            <div className='add-task-button-container'>
-                <Button onClick={handleAddTaskClick}>Adicionar</Button>
-            </div>
-        </div>
-    ); 
+	return (
+		<div className="add-task-container">
+			<input
+				onChange={handleInputChange}
+				value={inputData}
+				className="add-task-input"
+				type="text"
+			/>
+			<div className="add-task-button-container">
+				<Button onClick={handleAddTaskClick}>Adicionar</Button>
+			</div>
+		</div>
+	);
 };
- 
+
 export default AddTask;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import Task from './Task';
+import React from "react";
 
-const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion}) => {
-    return (  
-        <>
-            {tasks.map((task) => (
-                <Task 
-                    task={task} 
-                    handleTaskClick = {handleTaskClick} 
-                    handleTaskDeletion = {handleTaskDeletion}
-                    />
-           ))};
-        </>
-    );
-}
- 
+import Task from "./Task";
+
+const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion }) => {
+	return (
+		<>
+			{tasks.map((task) => (
+				<Task
+					key={task.id}
+					task={task}
+					handleTaskClick={handleTaskClick}
+					handleTaskDeletion={handleTaskDeletion}
+				/>
+			))}
+		</>
+	);
+};
+
 export default Tasks;
